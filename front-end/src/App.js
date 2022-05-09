22f8b12029291791b7aa696c6c1b12aa10ab12fb
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-09 09:40:12 +0700

    Adding React Toastify

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/react-toastify.css';
 
 function App() {
 	return (
 		<div className="App">
+			<ToastContainer />
 			<h2>Caeser's Tours</h2>
 		</div>
 	);
