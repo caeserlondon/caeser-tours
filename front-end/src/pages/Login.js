2791ff6572a24c1572744a72699c9f9d86a4823a
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-09 18:51:02 +0700

    Setting up the Redux for the Login

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -1,18 +1,19 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-
+import React, { useState, useEffect } from 'react';
 import {
-	MDBIcon,
 	MDBCard,
 	MDBCardBody,
-	MDBCardFooter,
 	MDBInput,
+	MDBCardFooter,
 	MDBValidation,
 	MDBValidationItem,
-	MDBSpinner,
 	MDBBtn,
-	MDBFooter,
+	MDBIcon,
+	MDBSpinner,
 } from 'mdb-react-ui-kit';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
+import { login } from '../redux/features/authSlice';
 
 const initialState = {
 	email: '',
@@ -21,13 +22,21 @@ const initialState = {
 
 const Login = () => {
 	const [formValue, setFormValue] = useState(initialState);
-
+	const { loading, error } = useSelector((state) => ({ ...state.auth }));
 	const { email, password } = formValue;
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		error && toast.error(error);
+	}, [error]);
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
+		if (email && password) {
+			dispatch(login({ formValue, navigate, toast }));
+		}
 	};
-
 	const onInputChange = (e) => {
 		let { name, value } = e.target;
 		setFormValue({ ...formValue, [name]: value });
