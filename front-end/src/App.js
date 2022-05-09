db72c737b50bd2f1082ba0bc988424923648cb29
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-09 10:08:46 +0700

    Adding the Router & the Pages Routes to the App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
+
 import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/react-toastify.css';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.js';
+import Login from './pages/Login.js';
+import Register from './pages/Register.js';
 
 function App() {
 	return (
-		<div className="App">
-			<ToastContainer />
-			<h2>Caeser's Tours</h2>
-		</div>
+		<BrowserRouter>
+			<div className="App">
+				<ToastContainer />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/register" element={<Register />} />
+				</Routes>
+			</div>
+		</BrowserRouter>
 	);
 }
 
