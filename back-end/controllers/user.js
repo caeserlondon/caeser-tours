af6ee0a6c5fa6e93e5005585629ae24fc65febcb
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-10 09:37:39 +0700

    Fixing the signup Functionality

--- back-end/controllers/user.js
+++ back-end/controllers/user.js
@@ -6,13 +6,12 @@ import UserModal from '../models/user.js';
 const secret = 'test';
 
 export const signup = async (req, res) => {
-	const { firstName, lastName, email, password } = req.body;
-
+	const { email, password, firstName, lastName } = req.body;
 	try {
 		const oldUser = await UserModal.findOne({ email });
 
 		if (oldUser) {
-			return res.status(400).json({ messege: 'User already exsists' });
+			return res.status(400).json({ message: 'User already exists' });
 		}
 
 		const hashedPassword = await bcrypt.hash(password, 12);
@@ -26,10 +25,9 @@ export const signup = async (req, res) => {
 		const token = jwt.sign({ email: result.email, id: result._id }, secret, {
 			expiresIn: '1h',
 		});
-
 		res.status(201).json({ result, token });
 	} catch (error) {
-		res.status(500).json({ messege: 'Somthing went wrong' });
+		res.status(500).json({ message: 'Something went wrong' });
 		console.log(error);
 	}
 };
