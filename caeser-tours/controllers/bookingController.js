87135b65e2685f252782972ff9013445f17580bd
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2025-11-16 10:00:55 +0700

    Updated the price currency

--- caeser-tours/controllers/bookingController.js
+++ caeser-tours/controllers/bookingController.js
@@ -28,7 +28,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
           `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`
         ],
         amount: tour.price * 100,
-        currency: 'usd',
+        currency: 'gbp',
         quantity: 1
       }
     ]
