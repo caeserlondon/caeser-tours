e801e57be13e700178883be77796728f3101bbbc
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2025-11-20 16:25:58 +0700

    Prepared for publishing

--- front-end/src/redux/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API = axios.create({
-	baseURL: 'http://localhost:5050',
-});
-
-export const signIn = (formData) => API.post('/users/signin', formData);
-export const signUp = (formData) => API.post('/users/signup', formData);
